cd3beee57dc238b8668c7c1b46f4c8d69cc420ec
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-09-30 22:14:35 +0300

    home: add partners section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  partners = [
+    {
+      name: 'Citi Bank',
+      image: 'assets/images/partner1.svg'
+    },
+    {
+      name: 'Visa',
+      image: 'assets/images/partner2.svg'
+    },
+    {
+      name: 'Master Card',
+      image: 'assets/images/partner3.svg'
+    },
+    {
+      name: 'TradingView',
+      image: 'assets/images/partner4.svg'
+    },
+    {
+      name: 'HSBC Bank',
+      image: 'assets/images/partner5.svg'
+    },
+    {
+      name: 'NYSE',
+      image: 'assets/images/partner6.svg'
+    }
+  ]
 }
