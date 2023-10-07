87baa2d6e05af259c2535d883e0457e827eb31b3
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-10-07 20:56:44 +0300

    finish verify email page

--- src/app/verify-email/verify-email.component.ts
+++ src/app/verify-email/verify-email.component.ts
@@ -4,12 +4,27 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-verify-email',
   templateUrl: './verify-email.component.html',
-  styleUrls: ['./verify-email.component.css']
+  styleUrls: ['./verify-email.component.css'],
 })
-export class VerifyEmailComponent implements OnInit{
-  constructor(
-    public authService: AuthService
-  ) { }
+export class VerifyEmailComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+  btn: HTMLButtonElement;
   ngOnInit() {
+    this.btn = document.getElementById('resendEmailBtn') as HTMLButtonElement;
+  }
+  sendVerificationEmail() {
+    this.authService.SendVerificationMail();
+    this.btn.disabled = true;
+    this.btn.classList.add('bg-gray-dark');
+    for (let i = 0; i < 60; i++) {
+      setTimeout(() => {
+        this.btn.innerHTML = `Resend Verification Email (${60 - i})`;
+      }, i * 1000);
+    }
+    setTimeout(() => {
+      this.btn.innerHTML = 'Resend Verification Email';
+      this.btn.disabled = false;
+      this.btn.classList.remove('bg-gray-dark');
+    }, 60000);
   }
 }
