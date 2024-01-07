a5d992f778779ea6ef0a90884779032fb01606ca
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-01-08 01:40:55 +0200

    add search functionality to favorites page

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { StockService } from '../services/stock.service';
 
 @Component({
   selector: 'app-favorites',
@@ -13,9 +14,11 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./favorites.component.css'],
 })
 export class FavoritesComponent {
-  constructor(public authService: AuthService, private renderer: Renderer2) {}
+  constructor(public authService: AuthService, private renderer: Renderer2, private stockAPI: StockService) {}
 
   @ViewChild('searchInput') searchInput: ElementRef;
+  searchQuery: string = '';
+  searchResults: any[] = [];
 
   removeFavorite(favorite: string) {
     this.authService.removeUserFavorite(favorite);
@@ -26,6 +29,8 @@ export class FavoritesComponent {
     document.getElementById('searchModal').classList.add('block');
     // focus on search input
     this.renderer.selectRootElement(this.searchInput.nativeElement).focus();
+    // const searchBar = document.getElementById('search-bar');
+    // searchBar.focus();
   }
 
   closeSearchModal() {
@@ -44,6 +49,17 @@ export class FavoritesComponent {
   clickout(event) {
     if (event.target.classList.contains('modal')) {
       this.closeSearchModal();
+    }
+  }
+
+  searchStocks() {
+    if (this.searchQuery.length >= 3) {
+      this.stockAPI.getStockSearchResults(this.searchQuery).then((data) => {
+        this.searchResults = data.data;
+        console.log(this.searchResults);
+      });
+    } else {
+      this.searchResults = [];
     }
   }
 }
--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -15,14 +15,14 @@ export class StockComponent implements OnInit {
   constructor(private stockAPI: StockService, private router: Router) {}
 
   ngOnInit(): void {
-    this.symbol = this.router.url.split('/')[2];
-    this.stockAPI.getStockData(this.symbol).then((data) => {
-      this.stockData = data;
-      console.log(this.stockData);
-    });
-    this.stockAPI.getStockLogo(this.symbol).then((data) => {
-      this.stockLogo = data;
-      console.log(this.stockLogo);
-    });
+    // this.symbol = this.router.url.split('/')[2];
+    // this.stockAPI.getStockData(this.symbol).then((data) => {
+    //   this.stockData = data;
+    //   console.log(this.stockData);
+    // });
+    // this.stockAPI.getStockLogo(this.symbol).then((data) => {
+    //   this.stockLogo = data;
+    //   console.log(this.stockLogo);
+    // });
   }
 }
